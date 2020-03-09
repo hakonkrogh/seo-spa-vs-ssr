797ac50ffe7280837d1ea6db6dda58dd54c5d651
Håkon Krogh <hakonhk@gmail.com>
2020-03-09 22:08:54 +0100

    Fixed GTM script

--- ssr/shared/layout.js
+++ ssr/shared/layout.js
@@ -35,7 +35,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           src="https://www.googletagmanager.com/ns.html?id=GTM-KMXBQB7"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"
+          style={{ display: "none", visibility: "hidden" }}
         ></iframe>
       </noscript>
       <style jsx global>{`
